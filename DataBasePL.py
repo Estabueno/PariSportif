604442c9bbe6fddbc59e49d69ce9d2f8b6fcdca1
salim <salim.mansouri15@gmail.com>
2024-02-12 23:51:03 +0100

    Base de donnée de la PL  1.0

---
+++ DataBasePL.py
@@ -0,0 +1,42 @@
+import sqlite3
+import Recover_Data_PL
+
+# Connexion à la base de données (si elle n'existe pas, elle sera créée) 
+conn = sqlite3.connect('Match_Pl.db')
+
+# Création d'un curseur pour exécuter des requêtes SQL 
+cur = conn.cursor()
+
+# Supprime les lignes de la table s'il elle a déjà été remplie.
+cur.execute('DELETE FROM Matchs')
+
+# Valider les modifications et fermer le curseur 
+conn.commit()
+
+# Supprime les pages inutiles
+cur.execute('VACUUM')
+
+# Création de la table si elle n'existe pas déjà 
+cur.execute('''CREATE TABLE IF NOT EXISTS Matchs(
+    Match INTEGER PRIMARY KEY, Equipe_Domicile TEXT, Cotes_Equipe_Domicile INTEGER, 
+    Cotes_Nul INTEGER, Equipe_Extérieur TEXT,
+    Cotes_Equipe_Extérieur INTEGER)''')
+
+home_team_value = Recover_Data_PL.Match_PL()['home_team']
+away_team_value = Recover_Data_PL.Match_PL()['away_team']
+draw_bets_value = Recover_Data_PL.Match_PL()['draw'] 
+home_bets_value = Recover_Data_PL.Match_PL()['home_bet'] 
+away_bets_value = Recover_Data_PL.Match_PL()['away_bet'] 
+
+for home, away, draw, home_bet, away_bet in zip(home_team_value, away_team_value, draw_bets_value, home_bets_value, away_bets_value):
+    # Insertion de données dans la table
+    cur.execute("INSERT INTO Matchs (Equipe_Domicile, Cotes_Equipe_Domicile, Cotes_Nul, Equipe_Extérieur, Cotes_Equipe_Extérieur) VALUES (?, ?, ?, ?, ?)", (home, home_bet, draw, away, away_bet)) 
+
+# Valider les modifications et fermer le curseur 
+conn.commit()
+
+# Valider les modifications et fermer le curseur conn.commit()
+cur.close()
+
+# Fermer la connexion à la base de données 
+conn.close()
--- Recover_Data_PL.py
+++ Recover_Data_PL.py
@@ -6,42 +6,49 @@ url = "https://www.enligne.parionssport.fdj.fr/paris-football/angleterre/premier
 # Envoyer une requête GET à l'URL
 response = requests.get(url)
 
-# Vérifier si la requête a réussi (code 200)
-if response.status_code == 200:
-    # Utiliser Beautiful Soup pour analyser le contenu de la page
-    soup = BeautifulSoup(response.text, 'html.parser')
+def Match_PL():
+    # Vérifier si la requête a réussi (code 200)
+    if response.status_code == 200:
+        # Utiliser Beautiful Soup pour analyser le contenu de la page
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Trouver la balise div avec la classe "psel-webapp-wrapper"
-    psel_webapp_wrapper = soup.find('div', class_='psel-webapp-wrapper')
+        # Trouver la balise div avec la classe "psel-webapp-wrapper"
+        psel_webapp_wrapper = soup.find('div', class_='psel-webapp-wrapper')
 
-    # Trouver toutes les balises div avec la classe "psel-market--3-columns" à l'intérieur de psel-webapp-wrapper
-    opponent_info = psel_webapp_wrapper.find_all('div', class_='psel-market--3-columns')
-    
-    # Initialiser les listes pour stocker les données 
-    home_team = []
-    draw = []
-    away_team = []
-    match_quotes = []
+        # Trouver toutes les balises div avec la classe "psel-market--3-columns" à l'intérieur de psel-webapp-wrapper
+        opponent_info = psel_webapp_wrapper.find_all('div', class_='psel-market--3-columns')
+        
+        # Initialiser les listes pour stocker les données 
+        home_team = []
+        away_team = []
+        draw = []
+        home_bet = []
+        away_bet = []
 
-    # Afficher le contenu de chaque balise div class="psel-market psel-market--3-columns psel-market__row"
-    for info in opponent_info:
-        # Extraire le texte des balises span avec la classe "psel-opponent__name"
-        teams = info.find_all('p', class_='psel-market__label')
+        # Afficher le contenu de chaque balise div class="psel-market psel-market--3-columns psel-market__row"
+        for info in opponent_info:
+            # Extraire le texte des balises span avec la classe "psel-opponent__name"
+            teams = info.find_all('p', class_='psel-market__label')
+            
+            # Extraire le texte des balises span avec la classe "psel-opponent__quote"
+            bets = info.find_all('span', class_='psel-outcome__data')
+            
+            # Stocker les données dans les listes correppondantes
+            home_team.append(teams[0].text.strip())
+            away_team.append(teams[2].text.strip())
+            
+            i = 0
+            
+            # Récupérer les cotes des matchs
+            for bet in bets:
+                if(i == 0):
+                    home_bet.append(bet.text.strip())
+                elif(i == 1):
+                    draw.append(bet.text.strip())
+                else:
+                    away_bet.append(bet.text.strip())
+                i+=1
         
-        # Extraire le texte des balises span avec la classe "psel-opponent__quote"
-        quotes = info.find_all('span', class_='psel-outcome__data')
-        
-        # Stocker les données dans les listes correppondantes
-        home_team.append(teams[0].text.strip())
-        draw.append(teams[1].text.strip())
-        away_team.append(teams[2].text.strip())
-        
-        # Récupérer les côtes des matchs
-        for quote in quotes:
-            match_quotes.append(quote.text.strip())
-        
-    print(home_team)
-    print(away_team)
-    print(match_quotes)
-else:
-    print("La requête GET a échoué avec le code :", response.status_code)
+        return  {'home_team': home_team, 'away_team': away_team, 'draw' : draw, 'home_bet' : home_bet, 'away_bet' : away_bet}
+    else:
+        print("La requête GET a échoué avec le code :", response.status_code)
